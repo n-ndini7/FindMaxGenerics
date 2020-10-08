6c7c77a8f291e875e197fc4f809461acbc0628f9
n-ndini7 <hp@RJs-PC>
2020-10-08 13:00:33 +0530

    [nandini] added master - welcome message

---
+++ src/main/java/com/FindMax/FindMax.java
@@ -0,0 +1,9 @@
+package com.FindMax;
+
+//Master branch - welcome message
+public class FindMax {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Find Maximum program!!");
+		System.out.println("---------------------------------");
+	}
+}
