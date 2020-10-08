42fb7230e88d083325738bf9f9c7592b8ab46658
n-ndini7 <hp@RJs-PC>
2020-10-08 15:28:49 +0530

    [nandini] Refactored - all UCs using generic method

--- src/test/java/FindMax/FindMaxTest.java
+++ src/test/java/FindMax/FindMaxTest.java
@@ -9,20 +9,59 @@ import com.FindMax.FindMax;
 public class FindMaxTest {
 
 	@Test
-	public void MaxAtPositionOne_ReturnSameNumber() {
+	public void StringMaxAtPositionOne_ReturnSameNumber() {
 		String max = FindMax.Maximum("Peach", "Apple", "Banana");
 		assertEquals("Peach", max);
 	}
 
 	@Test
-	public void MaxAtPositionTwo_ReturnSameNumber() {
+	public void StringMaxAtPositionTwo_ReturnSameNumber() {
 		String max = FindMax.Maximum("Apple", "Peach", "Banana");
 		assertEquals("Peach", max);
 	}
 
 	@Test
-	public void MaxAtPositionThree_ReturnSameNumber() {
+	public void StringMaxAtPositionThree_ReturnSameNumber() {
 		String max = FindMax.Maximum("Apple", "Banana", "Peach");
 		assertEquals("Peach", max);
 	}
+
+	// test cases for string
+	public void IntegerMaxAtPositionOne_ReturnSameNumber() {
+		int max = FindMax.Maximum(9, 6, 2);
+		assertEquals(9, max);
+	}
+
+	@Test
+	public void IntegerMaxAtPositionTwo_ReturnSameNumber() {
+		int max = FindMax.Maximum(6, 9, 2);
+		assertEquals(9, max);
+	}
+
+	@Test
+	public void IntegerMaxAtPositionThree_ReturnSameNumber() {
+		int max = FindMax.Maximum(6, 2, 9);
+		assertEquals(9, max);
+	}
+	// test cases for integer
+
+	@Test
+	public void FloatMaxAtPositionOne_ReturnSameNumber() {
+		Float max = FindMax.Maximum(9.9f, 6.6f, 2.2f);
+		assertEquals(9.9f, max, 0.0);
+	}
+
+	@Test
+	public void FloatMaxAtPositionTwo_ReturnSameNumber() {
+		Float max = FindMax.Maximum(6.6f, 9.9f, 2.2f);
+		assertEquals(9.9f, max, 0.0);
+	}
+
+	@Test
+	public void FloatMaxAtPositionThree_ReturnSameNumber() {
+		Float max = FindMax.Maximum(6.6f, 2.2f, 9.9f);
+		assertEquals(9.9f, max, 0.0);
+	}
+	// teset cases for float
+
 }
